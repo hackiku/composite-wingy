7860ae9ce354674f0203c0e0340062e3e307a1ff
hackiku <ivan@pipewriter.io>
2025-03-04 12:22:44 +0100

    fix dark mode

--- src/routes/(rnd)/learnforce/+page.svelte
+++ src/routes/(rnd)/learnforce/+page.svelte
@@ -1,7 +1,7 @@
 <!-- src/routes/(rnd)/learnforce/+page.svelte -->
 
 <script lang="ts">
-  import { ModeWatcher } from "mode-watcher";
+  // import { ModeWatcher } from "mode-watcher";
   import Nav from '$lib/pages/learnforce/Nav.svelte';
   import Hero from '$lib/pages/learnforce/Hero.svelte';
   import BottomNav from '$lib/pages/learnforce/BottomNav.svelte';
@@ -12,7 +12,7 @@
   <title>Learnforce - Learning development with the right amount of tech</title>
 </svelte:head>
 
-<ModeWatcher />
+<!-- <ModeWatcher /> -->
 
 <main class="min-h-screen font-serif relative">
   <Nav />
